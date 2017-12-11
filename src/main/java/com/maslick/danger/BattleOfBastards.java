0f2bd7a7c21adc5597a78f2ea6949040b4cba70f
Pavel Maslov <pavel.masloff@gmail.com>
2017-12-11 13:35:42 +0100

    fixed annotation processor version

--- src/main/java/com/maslick/danger/BattleOfBastards.java
+++ src/main/java/com/maslick/danger/BattleOfBastards.java
@@ -6,9 +6,9 @@ public class BattleOfBastards {
 
     public static void main(String[] args){
         // Ручное внедрение зависимостей
-//        Starks starks = new Starks();
-//        Boltons boltons = new Boltons();as
-//        war.report();
+        // Starks starks = new Starks();
+        // Boltons boltons = new Boltons();as
+        // war.report();
         BattleComponent component = DaggerBattleComponent.create();
         War war = component.getWar();
         war.prepare();
