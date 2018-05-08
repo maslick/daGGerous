41c15bd4727d575d08ef7761addeef5271454cdd
Pavel Maslov <pavel.maslov@ijs.si>
2018-05-08 17:46:22 +0200

    testing singletons

--- src/main/java/com/maslick/danger/Kinzhal.kt
+++ src/main/java/com/maslick/danger/Kinzhal.kt
@@ -7,23 +7,43 @@ import javax.inject.Qualifier
 import javax.inject.Singleton
 
 
+// Business logic
 interface Api {
     fun getWeather(city: String): String
 }
-class MyApi1(val httpClient: HttpClient): Api {
+
+class RealApi(val httpClient: HttpClient): Api {
     override fun getWeather(city: String) = "sunny"
 }
 
-class MyApi2 : Api {
+class FakeApi : Api {
     override fun getWeather(city: String) = "rainy"
 }
 
-class Cache(val file: File)
-class File(val context: Context)
-class Context
-class LoggingInterceptor
-class HttpClient(val cache: Cache, val logging: LoggingInterceptor)
+class Context {
+    init { println("initializing context") }
+}
 
+class File(val context: Context) {
+    init { println("initializing file system") }
+}
+
+class Cache(val file: File) {
+    init { println("initializing cache") }
+}
+
+class LoggingInterceptor {
+    init { println("initializing logging interceptor") }
+}
+
+class HttpClient(val cache: Cache, val logging: LoggingInterceptor) {
+    init { println("initializing http client") }
+}
+
+
+// Dagger config (qualifiers, modules, components)
+@Qualifier annotation class Real
+@Qualifier annotation class Stub
 
 @Module
 class ContextModule(private val context: Context) {
@@ -33,15 +53,19 @@ class ContextModule(private val context: Context) {
 
 @Module(includes = [ContextModule::class])
 class HttpModule {
+    @Singleton
     @Provides
     fun httpClient(cache: Cache, logging: LoggingInterceptor)= HttpClient(cache, logging)
 
+    @Singleton
     @Provides
     fun cache(file: File) = Cache(file)
 
+    @Singleton
     @Provides
     fun file(context: Context) = File(context)
 
+    @Singleton
     @Provides
     fun logging() = LoggingInterceptor()
 }
@@ -50,14 +74,14 @@ class HttpModule {
 class ApiModule {
     @Real
     @Provides
-    fun api(httpClient: HttpClient): Api = MyApi1(httpClient)
+    fun api(httpClient: HttpClient): Api = RealApi(httpClient)
 }
 
 @Module
 class FakeApiModule {
     @Stub
     @Provides
-    fun api(): Api = MyApi2()
+    fun api(): Api = FakeApi()
 }
 
 @Singleton
@@ -67,10 +91,8 @@ interface ApiComponent {
     @Stub fun fakeApi(): Api
 }
 
-@Qualifier annotation class Real
-@Qualifier annotation class Stub
 
-
+// Application
 object App {
     @JvmStatic
     fun main(args: Array<String>) {
@@ -78,12 +100,21 @@ object App {
                 .contextModule(ContextModule(Context()))
                 .build()
 
+        val realApi = component.api()
+        val stubApi = component.fakeApi()
+
+        println()
+
         val city1 = "Ljubljana"
-        val weather1 = component.api().getWeather(city1)
+        val weather1 = realApi.getWeather(city1)
         println("weather in $city1: $weather1")
 
         val city2 = "St. Pete"
-        val weather2 = component.fakeApi().getWeather(city2)
+        val weather2 = stubApi.getWeather(city2)
         println("weather in $city2: $weather2")
+
+        // testing the singletons
+        val realApi2 = component.api()
+        val stubApi2 = component.fakeApi()
     }
 }
